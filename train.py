77dcf9bbf4ba505c9025c94ceb6386cc2a78a6a1
mayor-of-geology <atolagbejoshua2@gmail.com>
2024-09-25 20:13:48 +0100

    remove aug pipeline

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 import numpy as np  
 import torch
 from ultralytics import YOLO
-from ultralytics.data.augment import Albumentations
-from ultralytics.utils import LOGGER, colorstr
 from argparse import ArgumentParser
 
 random_seed = 1234
@@ -26,41 +24,6 @@ def freeze_layer(trainer, num):
             v.requires_grad = False 
     print(f"{num_freeze} layers are freezed.")
 
-#Augmentation
-#-----------------------------------------------------------------------------------------------
-def __init__(self, p=1.0):
-    """Initialize the transform object for YOLO bbox formatted params."""
-    self.p = p
-    self.transform = None
-    prefix = colorstr("albumentations: ")
-    try:
-        import albumentations as A
-
-        # check_version(A.__version__, "1.0.3", hard=True)  # version requirement
-
-        # Transforms
-        T = [
-                # A.Rotate(limit = 10, p=0.5),
-                A.Blur(p=0.01),
-                A.MedianBlur(p=0.01),
-                A.ToGray(p=0.01),
-                A.CLAHE(p=0.01),
-                A.RandomBrightnessContrast(p=0.01),
-                A.RandomGamma(p=0.01),
-                A.ImageCompression(quality_lower=75, p=0.01),
-                A.MotionBlur(p=0.01, blur_limit=(3,9), always_apply=False),
-                # A.HorizontalFlip(p=0.01),
-        ]
-        self.transform = A.Compose(T, bbox_params=A.BboxParams(format="yolo", label_fields=["class_labels"]))
-
-        LOGGER.info(prefix + ", ".join(f"{x}".replace("always_apply=False, ", "") for x in T if x.p))
-    except ImportError:  # package not installed, skip
-        pass
-    except Exception as e:
-        LOGGER.info(f"{prefix}{e}")
-
-Albumentations.__init__ = __init__
-
 
 if __name__ == '__main__':
      
@@ -76,10 +39,12 @@ if __name__ == '__main__':
                         help='Batch size')
     parser.add_argument('--img_sz', nargs='?', type=int, default=640,
                         help='Image size')
-    parser.add_argument('--cfg', nargs='?', type=str, default='sa.yaml',
+    parser.add_argument('--cfg', nargs='?', type=str, default='data/sa.yaml',
                         help='Data configuration file')
     parser.add_argument('--model_path', nargs='?', type=str, default='models/FaciesSAM-x.pt',
                         help='Path to pretrained model')
+    parser.add_argument('--name', nargs='?', type=str, default='faciesam-s',
+                        help='model name')                        
 
     args = parser.parse_args()
 
@@ -94,6 +59,7 @@ if __name__ == '__main__':
         mode='train',
         epochs=args.epochs,
         batch=args.bs,
+        name=args.name+'_'+str(args.img_sz)+'_',
         imgsz=args.img_sz,
         save=True,
         optimizer='SGD',
@@ -101,7 +67,7 @@ if __name__ == '__main__':
         val=True,
         augment=args.aug,
         boxes=False,
-        patience=15,
+        patience=50,
         plots=True,
         fliplr= 0.5,
         mosaic= 1.0, #you can modify
@@ -109,3 +75,4 @@ if __name__ == '__main__':
         copy_paste= 0.3, #you can modify
         scale=0.9, #you can modify
     )
+
